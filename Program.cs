efe46b88ad159385884fd52fc30943816fe4f782
Eva Valkova <eva.n.valkova@gmail.com>
2026-02-14 23:09:14 +0200

    Log In and Sign In

--- Program.cs
+++ Program.cs
@@ -29,4 +29,21 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    string[] roles = { "Author", "Reader" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
+app.Run();
+
 app.Run();
